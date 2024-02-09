2b729eea58aa5a2945f720209543e61ab0c6535d
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2024-02-09 21:56:27 +0200

    Find EFI GOP framebuffer
    Implement function FindGOP that locates the EFI GOP and saves
    relevant information of the framebuffer to BootInfo.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <Uefi.h>
 #include <Guid/FileInfo.h>
+#include <Protocol/GraphicsOutput.h>
 #include <Protocol/LoadedImage.h>
 #include <Protocol/SimpleFileSystem.h>
 
@@ -9,6 +10,26 @@
 EFI_SYSTEM_TABLE *ST;
 EFI_HANDLE ImageHandle;
 BootInfo BootData;
+
+EFI_STATUS FindGOP(void)
+{
+	EFI_STATUS Status;
+
+	EFI_GUID GOPGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+	EFI_GRAPHICS_OUTPUT_PROTOCOL *GOP = NULL;
+	Status =
+		ST->BootServices->LocateProtocol(&GOPGuid, NULL, (void **)&GOP);
+	if (EFI_ERROR(Status))
+		return Status;
+
+	BootData.FBBase = (void *)GOP->Mode->FrameBufferBase;
+	BootData.FBSize = GOP->Mode->FrameBufferSize;
+	BootData.FBWidth = GOP->Mode->Info->HorizontalResolution;
+	BootData.FBHeight = GOP->Mode->Info->VerticalResolution;
+	BootData.FBPixelsPerScanLine = GOP->Mode->Info->PixelsPerScanLine;
+
+	return Status;
+}
 
 EFI_STATUS efi_main(EFI_HANDLE _ImageHandle, EFI_SYSTEM_TABLE *_ST)
 {
@@ -26,6 +47,10 @@ EFI_STATUS efi_main(EFI_HANDLE _ImageHandle, EFI_SYSTEM_TABLE *_ST)
 	UINT8 *KernelData = NULL;
 	UINTN KernelLength = 0;
 	Status = LoadFile(KernelFile, &KernelData, &KernelLength);
+	if (EFI_ERROR(Status))
+		return Status;
+
+	Status = FindGOP();
 	if (EFI_ERROR(Status))
 		return Status;
 
